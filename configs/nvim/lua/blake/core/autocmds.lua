3d96086d74ab6f6eb4dbf98fb5105240580ea02b
kgaurav07 <kgaurav07@users.noreply.github.com>
2026-04-19 16:45:11 +0530

    fix: markdown wrap off by default, toggle with <leader>mw

--- configs/nvim/lua/blake/core/autocmds.lua
+++ configs/nvim/lua/blake/core/autocmds.lua
@@ -1,7 +1,11 @@
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function()
-    vim.opt_local.wrap = true
+    vim.opt_local.wrap = false
     vim.opt_local.linebreak = true
+    -- Toggle wrap with <leader>mw when needed for long prose
+    vim.keymap.set("n", "<leader>mw", function()
+      vim.opt_local.wrap = not vim.opt_local.wrap:get()
+    end, { buffer = true, desc = "Toggle wrap (markdown)" })
   end,
 })
